418271402c6cfbb2f53ca4a1cb45db8e503bd943
dejankaradzinski <dejan.karadzinski@miraclemill.com>
2025-08-18 09:08:24 +0200

    User usage implementation: - Lambda function for creating metrics

--- cdk/lib/constructs/usage-analysis.ts
+++ cdk/lib/constructs/usage-analysis.ts
@@ -247,6 +247,46 @@ export class UsageAnalysis extends Construct {
       },
       logRetention: logs.RetentionDays.THREE_MONTHS,
     });
+
+    const usageHandler = new python.PythonFunction(this, "UsageHandler", {
+      entry: path.join(__dirname, "../../../backend/user_usage/"),
+      runtime: Runtime.PYTHON_3_13,
+      environment: {},
+      logRetention: logs.RetentionDays.THREE_MONTHS,
+    });
+
+    usageHandler.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: [
+				"athena:GetQueryExecution",
+				"athena:GetQueryResults",
+				"athena:StartQueryExecution",
+				"s3:GetBucketLocation",
+				"s3:PutObject",
+				"s3:ListBucket",
+				"s3:GetObject",
+				"glue:GetDatabase",
+				"athena:StartQueryExecution",
+				"athena:GetQueryExecution",
+				"athena:GetQueryResults",
+				"glue:GetTable",
+				"glue:GetTables",
+				"glue:GetDatabase",
+				"glue:GetDatabases",
+				"glue:GetPartition",
+				"glue:GetPartitions",
+				"dynamodb:GetRecords",
+				"dynamodb:ListStreams",
+				"dynamodb:GetShardIterator",
+				"dynamodb:DescribeStream",
+				"cognito-idp:AdminGetUser",
+				"cloudwatch:PutMetricData"
+        ],
+        resources: ["*"],
+      })
+    );
+
+
     exportHandler.role?.addToPrincipalPolicy(
       new iam.PolicyStatement({
         actions: ["dynamodb:ExportTableToPointInTime"],
